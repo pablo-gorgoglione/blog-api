97d31e6d1e1e1125bf898a61c72af2c2fa5e4543
pablo-gorgoglione <pablogorgoglione@gmail.com>
2022-01-09 23:33:33 -0300

    fix: add public routes to get comments

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,11 +1,10 @@
-const router = require("express").Router({ mergeParams: true });
-const commentController = require("../controllers/commentController");
+const router = require('express').Router({ mergeParams: true });
+const commentController = require('../controllers/commentController');
 
-router.post("/", commentController.createOne);
-router.post("/:idComment", commentController.createOne);
-router.delete("/:idComment", commentController.deleteOne);
-router.put("/:idComment", commentController.updateOne);
-router.get("/", commentController.getAllForOnePost);
-router.use("/:idComment/like", require("./likeRoutes"));
+router.post('/', commentController.createOne);
+router.post('/:idComment', commentController.createOne);
+router.delete('/:idComment', commentController.deleteOne);
+router.put('/:idComment', commentController.updateOne);
+router.use('/:idComment/like', require('./likeRoutes'));
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const passport = require('passport');
 const postController = require('../controllers/postController');
+const commentController = require('../controllers/commentController');
 
 router.use('/user', require('./userRoutes'));
 
-// original
 router.get('/post', postController.getAll);
 router.get('/post/:idPost', postController.getOne);
+router.get('/post/:idPost/comment', commentController.getAllForOnePost);
 
 router.use(
   '/post',
